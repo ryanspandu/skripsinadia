fd93242e32748ac75bf7b35c2852ba6c7a65914f
ryanspandu <1910036@gmail.com>
2020-12-31 13:07:46 +0700

    index update

--- web/index.php
+++ web/index.php
@@ -20,14 +20,14 @@
                     <img src="assets/img/logo.png" width="85px" height="100px" class="mt-2"/>
                     <div class="ml-4">
                         <h1 class="text-white mt-0 font-weight-bold">DINAS PUPR</h1>
-                        <p class="text-light">Kabupaten Bogor</p>
+                        <p class="text-light font-italic">Kabupaten Bogor</p>
                     </div>
                 </div>
                 <div class="col-12 border-bottom py-3 mt-5">
-                    <a href="index.php" class="text-white" style="font-size: 24px;">Data Karyawan</a>
+                    <a href="index.php" class="text-white" style="opacity: 0.5;font-size: 24px;">Data Karyawan</a>
                 </div>
                 <div class="col-12 border-bottom py-3 mt-3">
-                    <a href="tambah.php" class="text-white" style="font-size: 24px;">Tambah Karyawan</a>
+                    <a href="tambah.php" class="text-white" style="opacity: 0.5;font-size: 24px;">Tambah Karyawan</a>
                 </div>
             </div>
         </div>
