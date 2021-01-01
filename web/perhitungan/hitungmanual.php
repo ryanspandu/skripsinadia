2128dcbce343f5f10a7b406e9c7ee437f6c59c30
ryanspandu <1910036@gmail.com>
2021-01-01 16:25:33 +0700

    perhitungna manual selesai

--- web/perhitungan/hitungmanual.php
+++ web/perhitungan/hitungmanual.php
@@ -2,7 +2,7 @@
 include 'k-means.php';
 include 'moora.php';
 
-// dd($k_means);
+ //dd($hasil_moora);
 ?>
 
 <!DOCTYPE html>
@@ -38,9 +38,45 @@ include 'moora.php';
 </head>
 <body class="pb-5">
 
+<div class="container" style="margin-bottom: 30px;">
+    <h4>Perhitungan K-Means</h4>
+    <p>Inisialisasi Klaster</p>            
+    <table class="table table-custom table-bordered" style="max-width: 700px;">
+        <thead>
+        <tr>
+            <th class="nilai">Klaster</th>
+            <th class="nilai">Mutu Kerja</th>
+            <th class="nilai">Tanggung Jawab</th>
+            <th class="nilai">Inisiatif</th>
+            <th class="nilai">Kejujuran</th>
+            <th class="nilai">Absensi</th>
+        </tr>
+        </thead>
+        <tbody>
+ 
+        <tr>
+            <th class="nilai">Layak</th>
+            <td class="nilai"><?php echo $layak['mutu_kerja']; ?></td>
+            <td class="nilai"><?php echo $layak['tanggung_jawab']; ?></td>
+            <td class="nilai"><?php echo $layak['inisiatif']; ?></td>
+            <td class="nilai"><?php echo $layak['kejujuran']; ?></td>
+            <td class="nilai"><?php echo $layak['absensi']; ?></td>
+        </tr>
+        <tr>
+            <th class="nilai">Tidak Layak</th>
+            <td class="nilai"><?php echo $tidak_layak['mutu_kerja']; ?></td>
+            <td class="nilai"><?php echo $tidak_layak['tanggung_jawab']; ?></td>
+            <td class="nilai"><?php echo $tidak_layak['inisiatif']; ?></td>
+            <td class="nilai"><?php echo $tidak_layak['kejujuran']; ?></td>
+            <td class="nilai"><?php echo $tidak_layak['absensi']; ?></td>
+        </tr>
+        </tbody>
+    </table>
+</div>
+
 <div class="container">
-  <h4>Perhitungan K-Means</h4>
-  <p>Iterasi ke- <?php echo count($k_means['iterasi']); ?></p>            
+  <!-- <h4>Perhitungan K-Means</h4> -->
+  <p class="mt-5">Iterasi ke- <?php echo count($k_means['iterasi']); ?></p>            
   <table class="table table-custom table-bordered" style="max-width: 700px;">
     <thead>
       <tr>
@@ -76,6 +112,95 @@ include 'moora.php';
     </tbody>
   </table>
 </div>
+
+<div class="container" style="margin-top: 30px;">
+    <!-- <h4>Perhitungan K-Means</h4> -->
+    <p class="mt-5">Normalisasi MOORA Klaster Layak</p>            
+    <table class="table table-custom table-bordered" style="max-width: 700px;">
+      <thead>
+        <tr>
+          <th>Kode</th>
+          <th class="nilai">Mutu Kerja</th>
+          <th class="nilai">Tanggung Jawab</th>
+          <th class="nilai">Inisiatif</th>
+          <th class="nilai">Kejujuran</th>
+          <th class="nilai">Absensi</th>
+        </tr>
+      </thead>
+      <tbody>
+      <?php foreach($karyawan_moora['layak'] as $k){ ?>
+        <tr>
+          <td><?php echo $k['kode']; ?></td>
+          <td class="nilai"><?php echo $k['mutu_kerja']; ?></td>
+          <td class="nilai"><?php echo $k['tanggung_jawab']; ?></td>
+          <td class="nilai"><?php echo $k['inisiatif']; ?></td>
+          <td class="nilai"><?php echo $k['kejujuran']; ?></td>
+          <td class="nilai"><?php echo $k['absensi']; ?></td>
+        </tr>
+      <?php $j++; } ?>
+      </tbody>
+    </table>
+
+    <p style="margin-top: 30px;">Normalisasi MOORA Klaster Tidak Layak</p>            
+    <table class="table table-custom table-bordered" style="max-width: 700px;">
+      <thead>
+        <tr>
+          <th>Kode</th>
+          <th class="nilai">Mutu Kerja</th>
+          <th class="nilai">Tanggung Jawab</th>
+          <th class="nilai">Inisiatif</th>
+          <th class="nilai">Kejujuran</th>
+          <th class="nilai">Absensi</th>
+        </tr>
+      </thead>
+      <tbody>
+      <?php foreach($karyawan_moora['tidak_layak'] as $k){ ?>
+        <tr>
+          <td><?php echo $k['kode']; ?></td>
+          <td class="nilai"><?php echo $k['mutu_kerja']; ?></td>
+          <td class="nilai"><?php echo $k['tanggung_jawab']; ?></td>
+          <td class="nilai"><?php echo $k['inisiatif']; ?></td>
+          <td class="nilai"><?php echo $k['kejujuran']; ?></td>
+          <td class="nilai"><?php echo $k['absensi']; ?></td>
+        </tr>
+      <?php $j++; } ?>
+      </tbody>
+    </table>
+
+
+    <p style="margin-top: 30px;">Optimasi & Perangkingan MOORA</p>            
+    <table class="table table-custom table-bordered" style="max-width: 700px;">
+      <thead>
+        <tr>
+          <th>No.</th>
+          <th class="nilai">Kode</th>
+          <th class="nilai">Nama</th>
+          <th class="nilai">Optimasi</th>
+          <th class="nilai">Klaster</th>
+        </tr>
+      </thead>
+      <tbody>
+      <?php $i=1; foreach($hasil_moora['layak'] as $k){ ?>
+        <tr>
+          <td><?php echo $i++; ?></td>
+          <td><?php echo $k['kode']; ?></td>
+          <td class="nilai"><?php echo $k['nama']; ?></td>
+          <td class="nilai"><?php echo $k['optimasi']; ?></td>
+          <td class="nilai">Layak</td>
+        </tr>
+      <?php  } ?>
+      <?php foreach($hasil_moora['tidak_layak'] as $k){ ?>
+        <tr>
+          <td><?php echo $i; ?></td>
+          <td><?php echo $k['kode']; ?></td>
+          <td class="nilai"><?php echo $k['nama']; ?></td>
+          <td class="nilai"><?php echo $k['optimasi']; ?></td>
+          <td class="nilai">Tidak Layak</td>
+        </tr>
+      <?php  $i++; } ?>
+      </tbody>
+    </table>
+  </div>
 
 </body>
 </html>
