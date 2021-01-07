b59f67a71e777861b4d89ae08cdc4c04959619db
ryanspandu <1910036@gmail.com>
2021-01-07 19:06:19 +0700

    update

--- web/perhitungan/manual/moora_normalisasi_layak.php
+++ web/perhitungan/manual/moora_normalisasi_layak.php
@@ -4,6 +4,24 @@ include '../../conn.php';
 include '../datakaryawan.php';
 include '../k-means.php';
 
+function normalisasimoora($kolom){
+    $normalisasi=[];
+    $i=1;
+    foreach($kolom as $k){
+        $normalisasi['data'][$i] = pow($k,2);
+        $i++;
+    }
+    $normalisasi['sum'] = array_sum($normalisasi['data']);
+    $normalisasi['sqrt'] = sqrt($normalisasi['sum']);
+
+    $j=1;
+    foreach($kolom as $k){
+        $normalisasi['data'][$j] = $k/$normalisasi['sqrt'];
+        $j++;
+    }
+    return $normalisasi;
+}
+
 for($i=1; $i<=count($hasil_k_means['layak']); $i++){
     $layak_mutu_kerja['data'][$i] = $hasil_k_means['layak'][$i]['mutu_kerja'];
     $layak_tanggung_jawab['data'][$i] = $hasil_k_means['layak'][$i]['tanggung_jawab'];
@@ -35,6 +53,7 @@ for($i=1; $i<=count($hasil_k_means['layak']); $i++){
     for($j=1; $j<=count($layak_mutu_kerja['data']); $j++){
         if($j == count($layak_mutu_kerja['data'])){
             echo $layak_mutu_kerja['data'][$j].'&sup2;';
+            echo ' = '.normalisasimoora($layak_mutu_kerja['data'])['data'][$i];
         }else{
             echo $layak_mutu_kerja['data'][$j].'&sup2;'.' + ';
         }
@@ -55,6 +74,7 @@ for($i=1; $i<=count($hasil_k_means['layak']); $i++){
     for($j=1; $j<=count($layak_tanggung_jawab['data']); $j++){
         if($j == count($layak_tanggung_jawab['data'])){
             echo $layak_tanggung_jawab['data'][$j].'&sup2;';
+            echo ' = '.normalisasimoora($layak_tanggung_jawab['data'])['data'][$i];
         }else{
             echo $layak_tanggung_jawab['data'][$j].'&sup2;'.' + ';
         }
@@ -75,6 +95,7 @@ for($i=1; $i<=count($hasil_k_means['layak']); $i++){
     for($j=1; $j<=count($layak_inisiatif['data']); $j++){
         if($j == count($layak_inisiatif['data'])){
             echo $layak_inisiatif['data'][$j].'&sup2;';
+            echo ' = '.normalisasimoora($layak_inisiatif['data'])['data'][$i];
         }else{
             echo $layak_inisiatif['data'][$j].'&sup2;'.' + ';
         }
@@ -95,6 +116,7 @@ for($i=1; $i<=count($hasil_k_means['layak']); $i++){
     for($j=1; $j<=count($layak_kejujuran['data']); $j++){
         if($j == count($layak_kejujuran['data'])){
             echo $layak_kejujuran['data'][$j].'&sup2;';
+            echo ' = '.normalisasimoora($layak_kejujuran['data'])['data'][$i];
         }else{
             echo $layak_kejujuran['data'][$j].'&sup2;'.' + ';
         }
@@ -115,6 +137,7 @@ for($i=1; $i<=count($hasil_k_means['layak']); $i++){
     for($j=1; $j<=count($layak_absensi['data']); $j++){
         if($j == count($layak_absensi['data'])){
             echo $layak_absensi['data'][$j].'&sup2;';
+            echo ' = '.normalisasimoora($layak_absensi['data'])['data'][$i];
         }else{
             echo $layak_absensi['data'][$j].'&sup2;'.' + ';
         }
